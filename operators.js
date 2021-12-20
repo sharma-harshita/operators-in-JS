ee1d9ca106ecebb04762dd4da53c48f08169e9ac
harshita.sharma <harshita.sharma@asksid.ai>
2021-12-20 21:34:17 +0530

    bit operators

--- operators.js
+++ operators.js
@@ -81,3 +81,85 @@
 // console.log("Logical And &&", a);
 // console.log("Logical OR ||", b);
 // console.log("Logical Not !", c);
+
+
+// Ternary operator   
+{/* <condition>  ? <success/true output>  : <failure/false output> */}
+
+// var isRahulPresent = false;
+
+// var condition  =  10==12 ? "He is present" : "He is absent";
+
+// console.log(condition);
+
+
+// Bitwise Operators
+// Bitwise And - a&b
+// Bitwise OR - a|b
+// Bitwise XOR - a^b
+// Bitwise NOT - ~a
+// Left Shift - a << b
+// Sign Propagating Right Shift - a >> b
+// Zero fill Right shift - a >>> b
+
+
+//  Logic Gates
+// AND 
+
+// a b  c(AND)
+// 0 0      0
+// 0 1      0  
+// 1 0      0
+// 1 1      1
+
+
+// OR
+// a b  c(OR)
+// 0 0      0
+// 0 1      1
+// 1 0      1
+// 1 1      1
+
+
+// XOR 
+// a b  c(XOR)
+// 0 0      0
+// 0 1      1
+// 1 0      1
+// 1 1      0
+
+
+// NOT  
+
+// a    c(NOT)
+// 0    1
+// 1    0
+
+// 2 =  0000 0000 0000 0 0 1 0
+// (8bit converison)
+// 8 = 2 raised to the power of 3
+// 4 = 2 raised to the power of 2
+// 2 = 2 raised to the power of 1
+// 1 = 2 raised to the power of 0
+
+// Binary coversions of the numbers :
+// 5 = 0 1 0 1
+// 7 = 0 1 1 1
+// 3 = 0 0 1 1
+
+var a = 3;      // 0000 0000 0000 0000 0011
+var b = 2;      // 0010
+
+var c = a & b;  // 0010
+
+var d = a | b;  // 0011
+
+var e = a ^ b;  // 0001
+
+var f = ~a  //1100
+
+console.log("Bitwise & ", c);
+console.log("Bitwise | ", d);
+console.log("Bitwise ^ XOR ", e);
+console.log("Bitwise ~ NOT ", f);   // we predict the output should be 12 but it comes as -4 , reason behind is that JS works on 64 bit manipulation, so we can never predict the correct output.
+
